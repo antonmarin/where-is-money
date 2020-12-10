6c06cc95795213a34815a3e85738fac082298eb9
Anton Marin <antonaryo85@gmail.com>
2020-12-10 19:39:58 +0300

    docs

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     // Align versions of all Kotlin components
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
 
-    // Use the Kotlin JDK 8 standard library.
+    // Use the Kotlin standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
 
     // This dependency is used by the application.
